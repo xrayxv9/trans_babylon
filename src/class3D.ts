f488b68195ac502603d0e9d98ad39a44a561683a
Clement Morel <cmorel@3G3.42angouleme.fr>
2025-08-13 03:53:02 +0200

    push before sleeping

--- src/class3D.ts
+++ src/class3D.ts
@@ -1,13 +1,76 @@
 import * as Babylon from "@babylonjs/core";
 
-class Carte3D {
-  mesh: Babylon.AbstractMesh;
+type Card = {
+	value: number;
+	color: number;
+	texture: number;
+	textures: Babylon.AbstractMesh;
+};
 
-  constructor(mesh: Babylon.AbstractMesh) {
-    this.mesh = mesh;
-  }
+function Ale(num: number): number
+{
+	return Math.floor(Math.random() * num);
+}
 
-  tourner180() {
-    this.mesh.rotation.y += Math.PI; // Tourne de 180° autour de Y
-  }
+function check(toCheck: boolean[][])
+{
+	for (let i:number = 0; i <= 3; i++)
+	{
+		for (let y:number = 0; y <= 13; y++)
+			if (toCheck[i][y] == false)
+				return true;
+	}
+	return false;
+}
+
+export class Card3D{
+    private _bool: boolean[][];
+    public _deck: Card[];
+
+    constructor() {
+        this._bool = Array.from({ length: 4 }, () => Array(13).fill(false));
+		this._deck = [];
+		
+		this.shuffle();
+    }
+
+	shuffle()
+	{
+        this._bool = Array.from({ length: 4 }, () => Array(13).fill(false));
+		this._deck = [];
+
+		let color:number;
+		let value:number;
+
+		while (check(this._bool))
+		{
+			color = Ale(4);
+			value = Ale(13);
+			if (!this._bool[color][value])
+			{
+				this._bool[color][value] = true;
+				this._deck.push({
+					value: value, 
+					color: color,
+					texture: color * 13 + value,
+					textures: null as unknown as Babylon.AbstractMesh,
+				})
+			}
+		}
+	}
+
+	print()
+	{
+		for (let i:number = 0; i < 52; i++)
+		{
+			if (this._deck[i].color == 0)
+				console.log(i + " : coeur : " + this._deck[i].value + " value global : " + this._deck[i].texture);
+			else if (this._deck[i].color == 1)
+				console.log(i + " : carreau : " + this._deck[i].value + " value global : " + this._deck[i].texture);
+			else if (this._deck[i].color == 2)
+				console.log(i + " :  pique : " + this._deck[i].value + " value global : " + this._deck[i].texture);
+			else if (this._deck[i].color == 3)
+				console.log(i + " : trefle : " + this._deck[i].value + " value global : " + this._deck[i].texture);
+		}
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import "@babylonjs/loaders"
 import * as Babylon from "@babylonjs/core"
+import { Card3D } from './class3D'
 
 window.addEventListener("DOMContentLoaded", () => {
 	const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
@@ -12,25 +13,24 @@ window.addEventListener("DOMContentLoaded", () => {
 		const scene = new Babylon.Scene(engine);
 		const camera = new Babylon.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Babylon.Vector3(0,150,-350), scene);
 		camera.fov = 1.5;
+		const deck = new Card3D();
+		deck.print();
 
 		camera.beta += -0.11;
-
-		// camera.attachControl(canvas, true);
+		camera.attachControl(canvas, true);
 		const light = new Babylon.HemisphericLight("light", new Babylon.Vector3(0, 1, 0), scene);
 		light.diffuse = new Babylon.Color3(1, 1, 1);
-		const mesh = Babylon.SceneLoader.ImportMesh(null, "./", "blackjack_table.glb", scene);
-		const card1 = Babylon.SceneLoader.ImportMesh("", "./", "playing_cards.glb", scene,
-		function (meshes){
-				// while 
-				let i:number = 1;
-				// console.log(meshes.length + "coucou mon quoicoubaka\n");
-				while (i < meshes.length)
+		// const mesh = Babylon.SceneLoader.ImportMesh(null, "./", "blackjack_table.glb", scene);
+		const card1 = Babylon.SceneLoader.ImportMesh(null, "./", "playing_cards.glb", scene,
+		function(meshes) {
+				for (let i:number = 1; i <= 53; i++)
 				{
 					meshes[i].scaling = new Babylon.Vector3(500, 500, 500);
-					meshes[i].position = new Babylon.Vector3(0 + i * 5000, 0, 0);
-					meshes[i].rotation = new Babylon.Vector3(0, 0, Math.PI);
-					i++;
-					console.log("i : " + i);
+					meshes[i].position = new Babylon.Vector3(100, 3 * i, -25);
+					meshes[i].rotation = new Babylon.Vector3(Math.PI / 2, 0, 0);
+					deck._deck[i].textures = meshes[i];
+					if (i > 25)
+						meshes[i].isVisible = false;
 				}
 			}
 		);
