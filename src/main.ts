ce1d61ec96bc651aef5201663e093845d84ee81e
Clement Morel <cmorel@3G3.42angouleme.fr>
2025-09-25 16:41:14 +0200

    It works, I can make my slot machine rotate !

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import './style.css'
 import "@babylonjs/loaders"
 import * as Babylon from "@babylonjs/core"
-import { Card3D } from './class3D'
+
+export function Ale(num: number): number
+{
+	return Math.floor(Math.random() * num);
+}
 
 window.addEventListener("DOMContentLoaded", () => {
 	const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
@@ -11,33 +15,31 @@ window.addEventListener("DOMContentLoaded", () => {
 	const createScene = () =>
 	{
 		const scene = new Babylon.Scene(engine);
-		const camera = new Babylon.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Babylon.Vector3(0,150,-350), scene);
+		const camera = new Babylon.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Babylon.Vector3(0,0,0), scene);
 		camera.fov = 1.5;
-		const deck = new Card3D();
-		deck.print();
 
 		camera.beta += -0.11;
 		camera.attachControl(canvas, true);
 		const light = new Babylon.HemisphericLight("light", new Babylon.Vector3(0, 1, 0), scene);
 		light.diffuse = new Babylon.Color3(1, 1, 1);
-		// const mesh = Babylon.SceneLoader.ImportMesh(null, "./", "blackjack_table.glb", scene);
-		const card1 = Babylon.SceneLoader.ImportMesh(null, "./", "playing_cards.glb", scene,
-		function(meshes) {
-				for (let i:number = 1; i <= 53; i++)
-				{
-					meshes[i].scaling = new Babylon.Vector3(500, 500, 500);
-					meshes[i].position = new Babylon.Vector3(100, 3 * i, -25);
-					meshes[i].rotation = new Babylon.Vector3(Math.PI / 2, 0, 0);
-					deck._deck[i].textures = meshes[i];
-					if (i > 25)
-						meshes[i].isVisible = false;
-				}
-			}
-		);
 		return scene;
 	}
-
 	const scene = createScene();
+
+	Babylon.SceneLoader.ImportMesh(null, "./", "slot_machine.glb", scene, (meshes)=>{
+		let i:number = 0;
+		meshes.forEach(element => {
+			if (i == 11 || i == 12 || i == 10 || i==1)
+			{
+				const rotate = element.rotation.clone();
+				element.rotation = rotate.add(new Babylon.Vector3(Ale(19), 0, 0));				
+			}
+				// element.isVisible = false;
+			i++;
+			const rotate = element.rotation.clone();
+			element.rotation = rotate.add(new Babylon.Vector3(Math.PI, 0, 0));
+		});
+	});
 
 	engine.runRenderLoop(() => {
 		scene.render();
